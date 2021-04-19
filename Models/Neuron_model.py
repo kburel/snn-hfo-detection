09b02e00a65c7706b640e6ec7330d67b2c1fda94
kburel <74239138+kburel@users.noreply.github.com>
2021-04-19 19:09:09 +0200

    Add neuron and synapse models

---
+++ Models/Neuron_model.py
@@ -0,0 +1,92 @@
+from brian2.units import * 
+Neuron_model = {'model':
+'''
+            dImem/dt = (((Ith_clip / Itau_clip) * (Iin_clip  + Ia_clip - Ishunt_clip - Iahp_clip)) - Ith_clip - ((1 + ((Ishunt_clip + Iahp_clip - Ia_clip) / Itau_clip)) * Imem)) / (tau * ((Ith_clip/(Imem + Io)) + 1)) : amp (unless refractory)
+            dIahp/dt = (- Ithahp_clip - Iahp + 2*Io*(Iahp<=Io)) / (tauahp * (Ithahp_clip / Iahp + 1)) : amp # adaptation current
+            Ia = Iagain / (1 + exp(-(Imem - Iath) / Ianorm)) : amp  # postive feedback current
+            Iahp_clip = Iahp*(Imem>Io) + Io*(Imem<=Io)  : amp
+            
+            Itau_clip = Itau*(Imem>Io) + Io*(Imem<=Io)  : amp
+            Ith_clip = Ith*(Imem>Io) + Io*(Imem<=Io)    : amp
+            Iin_clip = clip(Iin+Iconst,Io, 1*amp) : amp
+            Ia_clip = Ia*(Imem>Io) + 2*Io*(Imem<=Io)    : amp
+            Ithahp_clip = Ithahp*(Iahp>Io) + Io*(Iahp<=Io) : amp
+            Ishunt_clip = clip(Ishunt, Io, Imem) : amp
+
+            tau = (Cmem * Ut) / (kappa * Itau_clip) : second        # Membrane time constant
+            kappa = (kn + kp) / 2 : 1
+
+            Inoise  : amp                                    # Noise due to mismatch
+
+            kn      : 1 (shared, constant)                   # subthreshold slope factor for nFETs
+            kp      : 1 (shared, constant)                   # subthreshold slope factor for pFETs
+            Ut      : volt (shared, constant)                # Thermal voltage
+            Io      : amp (shared, constant)                 # Dark current
+            Cmem    : farad (shared, constant)               # Membrane capacitance
+            Ispkthr : amp (constant)                         # Spiking threshold
+            Ireset  : amp (shared, constant)                 # Reset current
+            refP    : second    (shared, constant)           # refractory period (It is still possible to set it to False)
+            Ith     : amp (constant)                         # DPI threshold (low pass filter).
+            Itau    : amp (constant)                         # Leakage current
+            Iconst  : amp (constant)                         # Additional input current similar to constant current injection
+            Ishunt  : amp (constant)                         # Shunting inhibitory current (directly affects soma)
+            Ica     : amp (constant)
+         
+
+
+            tauahp = (Cahp * Ut) / (kappa * Itauahp) : second # time constant of adaptation
+            Iahpmax = (Ica / Itauahp) * Ithahp_clip : amp     # Ratio of currents through diffpair and adaptation block
+            Ithahp : amp (constant)
+            Itauahp : amp (constant)
+            Cahp : farad (constant)
+         
+
+
+            Iagain : amp (constant)
+            Iath : amp (constant)
+            Ianorm : amp (constant)
+
+            dA/dt = -A/taufr : 1
+            taufr : second
+
+            polarity : 1
+            x : 1         (constant)        # x location on 2d grid (only set it if you need it)
+            y : 1         (constant)        # y location on 2d grid
+            Iin = Ie0+Ii0 : amp # input currents
+            Ie0 : amp
+            Ii0 : amp
+''',
+'threshold':
+'''Imem > Ispkthr''',
+'reset':
+'''
+Imem = Ireset;
+                  Iahp += Iahpmax;
+                  A += 1;
+                  
+''',
+'refractory': 'refP',
+'parameters':
+{
+'Inoise' : '0.5 * pamp',
+'Iconst' : '0.5 * pamp',
+'kn' : '0.75',
+'kp' : '0.66',
+'Ut' : '25. * mvolt',
+'Io' : '0.5 * pamp',
+'Cmem' : '1.5 * pfarad',
+'Iath' : '0.5 * namp',
+'Iagain' : '50. * pamp',
+'Ianorm' : '10. * pamp',
+'Ica' : '2. * pamp',
+'Itauahp' : '1. * pamp',
+'Ithahp' : '1. * pamp',
+'Cahp' : '1. * pfarad',
+'Ishunt' : '0.5 * pamp',
+'Ispkthr' : '1. * namp',
+'Ireset' : '0.6 * pamp',
+'Ith' : '0.9 * pamp',
+'Itau' : '8. * pamp',
+'refP' : '1. * msecond',
+}
+}
---
+++ Models/Synapse_model.py
@@ -0,0 +1,50 @@
+from brian2.units import * 
+Synapse_model = {'model':
+'''
+        dI_syn/dt = (-I_syn - I_gain + 2*Io_syn*(I_syn<=Io_syn))/(tausyn*((I_gain/I_syn)+1)) : amp (clock-driven)
+
+        Iin{input_number}_post = I_syn *  sign(weight)  : amp (summed)
+
+        weight : 1
+        w_plast : 1
+
+        I_gain = Io_syn*(I_syn<=Io_syn) + I_th*(I_syn>Io_syn) : amp
+        Itau_syn = Io_syn*(I_syn<=Io_syn) + I_tau*(I_syn>Io_syn) : amp
+
+        baseweight : amp (constant)     # synaptic gain
+        tausyn = Csyn * Ut_syn /(kappa_syn * Itau_syn) : second
+        kappa_syn = (kn_syn + kp_syn) / 2 : 1
+
+        Iw = abs(weight) * baseweight  : amp
+
+        I_tau       : amp (constant)
+        I_th        : amp (constant)
+        kn_syn       : 1 (constant)
+        kp_syn       : 1 (constant)
+        Ut_syn       : volt (constant)
+        Io_syn       : amp (constant)
+        Csyn         : farad (constant)
+          ''',
+'on_pre':
+'''
+
+        I_syn += Iw * w_plast * I_gain / (Itau_syn * ((I_gain/I_syn)+1))
+          ''',
+'on_post':
+'''
+   
+''',
+'parameters':
+{
+'Io_syn' : '0.5 * pamp',
+'kn_syn' : '0.75',
+'kp_syn' : '0.66',
+'Ut_syn' : '25. * mvolt',
+'Csyn' : '1.5 * pfarad',
+'I_tau' : '10. * pamp',
+'I_th' : '10. * pamp',
+'I_syn' : '0.5 * pamp',
+'w_plast' : '1',
+'baseweight' : '7. * pamp',
+}
+}
