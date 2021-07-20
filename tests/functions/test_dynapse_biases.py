a7e1d44e609efee55cf261c1048463165ce25773
Jan Nils Ferner <contact@jnferner.com>
2021-07-20 12:17:14 +0200

    Write tests for getTau

---
+++ tests/functions/test_dynapse_biases.py
@@ -0,0 +1,20 @@
+import pytest
+from SNN_HFO_iEEG.Functions.Dynapse_biases_functions import *
+
+@pytest.fixture
+def current():
+    return [1]
+
+@pytest.fixture
+def tau():
+    return [1]
+@pytest.mark.parametrize(
+    "current,expected_tau",
+    [(1, 5.35e-14),
+    (1e-4, 5.35e-10),
+    (1.5e-4, 3.57e-10),
+    pytest.param(0, None, marks=pytest.mark.xfail),
+    pytest.param(-1, None, marks=pytest.mark.xfail)],
+)
+def test_tau_is_collected(current, expected_tau):
+    assert expected_tau == pytest.approx(getTau(current))
