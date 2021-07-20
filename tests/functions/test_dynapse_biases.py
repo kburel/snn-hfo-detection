6539ea3c36602a831e50bb7bc6f2296cdb2d9ff9
Jan Nils Ferner <contact@jnferner.com>
2021-07-20 15:51:34 +0200

    Run autoPEP8

--- tests/functions/test_dynapse_biases.py
+++ tests/functions/test_dynapse_biases.py
@@ -1,44 +1,46 @@
 import pytest
 from SNN_HFO_iEEG.Functions.Dynapse_biases_functions import *
 
+
 @pytest.mark.parametrize(
     "current,expected_tau",
     [(1, 5.35e-14),
-    (1e-4, 5.35e-10),
-    (1.5e-4, 3.57e-10),
-    (-1, -5.35e-14)]
+     (1e-4, 5.35e-10),
+     (1.5e-4, 3.57e-10),
+     (-1, -5.35e-14)]
 )
 def test_get_tau(current, expected_tau):
     actual_tau = getTau(current)
     assert expected_tau == pytest.approx(actual_tau)
 
+
 def test_get_tau_raises_error_on_zero_current():
     with pytest.raises(ZeroDivisionError):
-         getTau(0)
+        getTau(0)
+
 
 @pytest.mark.parametrize(
     "tau,expected_current",
     [(1, 5.35e-14),
-    (1e-4, 5.35e-10),
-    (1.5e-4, 3.57e-10),
-    (-1, -5.35e-14),
-    (0, 2.390625e-05)] # Only difference to get_tau
+     (1e-4, 5.35e-10),
+     (1.5e-4, 3.57e-10),
+     (-1, -5.35e-14),
+     (0, 2.390625e-05)]  # Only difference to get_tau
 )
 def test_get_tau_current(tau, expected_current):
     actual_current = getTauCurrent(tau)
     assert expected_current == pytest.approx(actual_current)
 
+
 def test_get_tau_current_raises_error_when_vector_is_specified():
     with pytest.raises(TypeError):
-         getTauCurrent(1, vector=True)
+        getTauCurrent(1, vector=True)
+
 
 @pytest.mark.parametrize(
     "mean_tau,std_tau,expected_means",
     [(0, 0, (2.390625e-5, 0, 2.390625e-5, 2.390625e-5)),
-    (1, 1, (5.357142857142858e-11, 1.1953111607142857e-05, 2.678571428571429e-11, 2.390625e-05)),
-    ]
+     (1, 1, (5.357142857142858e-11, 1.1953111607142857e-05,
+      2.678571428571429e-11, 2.390625e-05)),
+     ]
 )
-# Seems to be unused...
-def test_get_mean_std_currents(mean_tau,std_tau,expected_means):
-    actual_means = get_mean_std_currents(mean_tau, std_tau)
-    assert expected_means == pytest.approx(actual_means)
--- tests/functions/test_filter_functions.py
+++ tests/functions/test_filter_functions.py
@@ -2,29 +2,34 @@ from decimal import DivisionByZero
 import pytest
 from SNN_HFO_iEEG.Functions.Filter_functions import *
 
+
 @pytest.mark.parametrize(
     "lowcut, highcut, fs, expected_coefficients",
-    [(0.4, 0.4, 1, ([0, 0, 0, 0, 0, 0, 0, 0],[0]))]
+    [(0.4, 0.4, 1, ([0, 0, 0, 0, 0, 0, 0, 0], [0]))]
 )
 def test_butter_bandpass(lowcut, highcut, fs, expected_coefficients):
     actual_a, actual_b = butter_bandpass(lowcut, highcut, fs)
 
-    to_np_array = lambda arr: np.array([pytest.approx(_) for _ in arr])
+    def to_np_array(arr): return np.array([pytest.approx(_) for _ in arr])
     expected_a, expected_b = to_np_array(expected_coefficients)
     print(expected_a)
     assert np.all(expected_a == actual_a)
     print(actual_b)
     assert np.all(expected_b == actual_b)
 
+
 def test_butter_bandpass_raises_error_when_fs_is_zero():
     with pytest.raises(ZeroDivisionError):
         butter_bandpass(lowcut=1, highcut=1, fs=0)
+
 
 def test_butter_bandpass_raises_error_when_cut_is_zero():
     with pytest.raises(ValueError):
         butter_bandpass(lowcut=0, highcut=1, fs=1)
 
+
 _CUT_TO_FS_RATIO_LIMIT = 0.5
+
 
 @pytest.mark.parametrize(
     "fs",
@@ -36,6 +41,7 @@ def test_butter_bandpass_raises_error_when_cut_to_fs_ratio_is_too_big(fs):
 
     with pytest.raises(ValueError):
         butter_bandpass(lowcut=cut, highcut=cut, fs=fs)
+
 
 @pytest.mark.parametrize(
     "fs",
