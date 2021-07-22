d938049f6db0f2d35b0b93cc1a982c66dba4e984
Jan Nils Ferner <contact@jnferner.com>
2021-07-22 11:38:19 +0200

    Run autopep8

--- tests/functions/test_dynapse_biases.py
+++ tests/functions/test_dynapse_biases.py
@@ -2,6 +2,7 @@ import pytest
 from SNN_HFO_iEEG.Functions.Dynapse_biases_functions import *
 from tests.utility import *
 
+
 @pytest.mark.parametrize(
     'current,expected_tau',
     [(1, 5.35e-14),
@@ -13,9 +14,11 @@ def test_get_tau(current, expected_tau):
     actual_tau = getTau(current)
     assert expected_tau == pytest.approx(actual_tau)
 
+
 def test_get_tau_raises_error_on_zero_current():
     with pytest.raises(ZeroDivisionError):
         getTau(0)
+
 
 @pytest.mark.parametrize(
     'tau,expected_current',
@@ -29,10 +32,12 @@ def test_get_tau_current(tau, expected_current):
     actual_current = getTauCurrent(tau)
     assert expected_current == pytest.approx(actual_current)
 
+
 @pytest.mark.filterwarnings('ignore::DeprecationWarning')
 def test_get_tau_current_raises_error_when_vector_is_specified_without_passing_vector():
     with pytest.raises(TypeError):
         getTauCurrent(1, vector=True)
+
 
 @pytest.mark.parametrize(
     'tau,expected_current',
