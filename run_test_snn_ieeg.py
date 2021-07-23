a516766b07ae9117ff19c4ac46f527134b129c19
Jan Nils Ferner <contact@jnferner.com>
2021-07-23 13:11:52 +0200

    Move param loading into own file

--- run_test_snn_ieeg.py
+++ run_test_snn_ieeg.py
@@ -1,6 +1,4 @@
-from typing import NamedTuple
 import os
-import warnings
 import argparse
 import scipy.io as sio
 from brian2.units import second
@@ -10,26 +8,13 @@ from snn_hfo_ieeg.functions.signal_to_spike import *
 from snn_hfo_ieeg.functions.hfo_detection import *
 from snn_hfo_ieeg.stages.filter import filter_stage
 from snn_hfo_ieeg.stages.snn import snn_stage
+from snn_hfo_ieeg.stages.parameter_loading import load_network_parameters
 
-_PACKAGE_NAME = 'snn_hfo_ieeg'
 
 _SAMPLING_FREQUENCY = 2000
 
 
 def run_hfo_detection(data_path, hfo_cb):
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    parameters_path = os.path.join(_PACKAGE_NAME, 'parameters')
-    snn_models_path = os.path.join(_PACKAGE_NAME, 'models')
-
-    # Load SNN parameters, neuron and synapse models
-    neuron_model_path = os.path.join(snn_models_path, 'neuron')
-    synapse_model_path = os.path.join(snn_models_path, 'synapse')
-
-    adm_parameters = sio.loadmat(os.path.join(
-        parameters_path, 'adm.mat'))
-    network_parameters = sio.loadmat(
-        os.path.join(parameters_path, 'network.mat'))
-
     # Select Data from a single patient
     patient = 1
     # interval
@@ -37,6 +22,8 @@ def run_hfo_detection(data_path, hfo_cb):
 
     file_name = f'P{patient}/P{patient}I{current_interval}.mat'
     interval = sio.loadmat(os.path.join(data_path, file_name))
+
+    network_parameters = load_network_parameters()
 
     num_channels = interval['chb'].shape[0]
     for channel in range(num_channels):
@@ -50,15 +37,15 @@ def run_hfo_detection(data_path, hfo_cb):
             wideband_signal=wideband_signal,
             sampling_frequency=_SAMPLING_FREQUENCY,
             signal_time=signal_time,
-            adm_parameters=adm_parameters)
+            adm_parameters=network_parameters.adm_parameters)
 
         extra_simulation_time = 0.050
         duration = np.max(signal_time) + extra_simulation_time
 
         spike_monitor_hidden = snn_stage(filtered_spikes=filtered_spikes,
                                          network_parameters=network_parameters,
-                                         neuron_model_path=neuron_model_path,
-                                         synapse_model_path=synapse_model_path,
+                                         neuron_model_path=network_parameters.neuron_model_path,
+                                         synapse_model_path=network_parameters.synapse_model_path,
                                          duration=duration)
         # ==================================
         # Readout stage
---
+++ snn_hfo_ieeg/stages/parameter_loading.py
@@ -0,0 +1,30 @@
+import os
+from typing import NamedTuple
+import scipy.io as sio
+_PACKAGE_NAME = 'snn_hfo_ieeg'
+
+class NetworkParameters(NamedTuple):
+    neuron_model_path: str
+    synapse_model_path: str
+    adm_parameters: dict
+    network_parameters: dict
+
+def load_network_parameters():
+    parameters_path = os.path.join(_PACKAGE_NAME, 'parameters')
+    snn_models_path = os.path.join(_PACKAGE_NAME, 'models')
+
+    # Load SNN parameters, neuron and synapse models
+    neuron_model_path = os.path.join(snn_models_path, 'neuron')
+    synapse_model_path = os.path.join(snn_models_path, 'synapse')
+
+    adm_parameters = sio.loadmat(os.path.join(
+        parameters_path, 'adm.mat'))
+    network_parameters = sio.loadmat(
+        os.path.join(parameters_path, 'network.mat'))
+        
+    return NetworkParameters(
+        neuron_model_path=neuron_model_path,
+        synapse_model_path=synapse_model_path,
+        adm_parameters=adm_parameters,
+        network_parameters=network_parameters
+    )
