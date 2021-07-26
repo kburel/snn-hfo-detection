a656e70275c3a837fef0a7605f26a9251ec9f5de
Jan Nils Ferner <contact@jnferner.com>
2021-07-26 16:54:58 +0200

    Fix inconsistency with original implementation

--- snn_hfo_ieeg/stages/snn/concatenation.py
+++ snn_hfo_ieeg/stages/snn/concatenation.py
@@ -9,8 +9,8 @@ def _concatenate_excitatory_and_inhibitory_for_input_pair(hidden_neuron_count, g
     excitatory_array, inhibitory_array = generate_array_cb(hidden_neuron_count)
     half_point = len(excitatory_array) // 2
     first_input_to_first_half_of_hidden = excitatory_array[:half_point]
-    first_input_to_second_half_of_hidden = inhibitory_array[:half_point]
-    second_input_to_first_half_of_hidden = inhibitory_array[half_point:]
+    first_input_to_second_half_of_hidden = inhibitory_array[half_point:]
+    second_input_to_first_half_of_hidden = inhibitory_array[:half_point]
     second_input_to_second_half_of_hidden = excitatory_array[half_point:]
     return np.concatenate([
         first_input_to_first_half_of_hidden,
@@ -22,10 +22,10 @@ def _concatenate_excitatory_and_inhibitory_for_input_pair(hidden_neuron_count, g
 def concatenate_excitatory_and_inhibitory_with_generator_function(input_neuron_count, hidden_neuron_count, generate_array_cb):
     if _is_odd(input_neuron_count):
         raise ValueError(
-            f'input_neuron_count must be a positive, even number but was odd. Actual value: {input_neuron_count}')
+            f'input_neuron_count must be a positive, even number, but got: {input_neuron_count}')
     if _is_odd(hidden_neuron_count):
         raise ValueError(
-            f'hidden_neuron_count must be a positive, even number but was odd. Actual value: {hidden_neuron_count}')
+            f'hidden_neuron_count must be a positive, even number, but got: {hidden_neuron_count}')
     input_pair_count = input_neuron_count // 2
     weights = [_concatenate_excitatory_and_inhibitory_for_input_pair(_, generate_array_cb)
                for _
