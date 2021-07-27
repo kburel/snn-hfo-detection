0d4c58beb05def5472e0af1185f341c6430bc2ae
Jan Nils Ferner <contact@jnferner.com>
2021-07-27 15:22:54 +0200

    Use data structures in tests

--- tests/functions/test_hfo_detection.py
+++ tests/functions/test_hfo_detection.py
@@ -1,18 +1,54 @@
 import pytest
-from snn_hfo_ieeg.functions.hfo_detection import *
+from snn_hfo_ieeg.functions.hfo_detection import Periods, detect_hfo, HfoDetection, PlottingData
 from tests.utility import *
 
 
 @pytest.mark.parametrize(
     'duration, spike_monitor, original_time_vector, step_size, window_size, expected_hfo_detection',
-    [(0, [0], np.array([0]), 0.1, 0.1, {'total_hfo': 0,
-                                        'time': [0], 'signal': [0], 'periods_hfo': [[], []]}),
-     (1, [0.5], np.array([0.5]), 0.1, 0.5, {'total_hfo': 1,
-                                            'time': [0.5], 'signal': [1], 'periods_hfo': [[0.5], []]}),
-     (-1, [-0.5], np.array([-0.5]), -0.1, -0.05, {'total_hfo': 0,
-                                                  'time': [-0.5], 'signal': [0], 'periods_hfo': [[], []]}),
-     (0.001, np.arange(0, 300, 5e-4), [0.2, 0.3], 0.01, 0.05, {'total_hfo': 0,
-                                                               'time': [0.2, 0.3], 'signal': [0, 0], 'periods_hfo': [[], []]})]
+    [(0, [0], np.array([0]), 0.1, 0.1, HfoDetection(
+        total_amount=0,
+        frequency=0,
+        plotting_data=PlottingData(
+            detections=[0],
+            analyzed_times=[0],
+            periods=Periods(
+                start=[],
+                stop=[]
+            )
+        ))),
+     (1, [0.5], np.array([0.5]), 0.1, 0.5, HfoDetection(
+         total_amount=1,
+         frequency=1,
+         plotting_data=PlottingData(
+             detections=[1],
+             analyzed_times=[0.5],
+             periods=Periods(
+                 start=[0.5],
+                 stop=[0.5]
+             )
+         ))),
+     (-1, [-0.5], np.array([-0.5]), -0.1, -0.05, HfoDetection(
+         total_amount=0,
+         frequency=0,
+         plotting_data=PlottingData(
+             detections=[0],
+             analyzed_times=[-0.5],
+             periods=Periods(
+                 start=[],
+                 stop=[]
+             )
+         ))),
+     (0.001, np.arange(0, 300, 5e-4), [0.2, 0.3], 0.01, 0.05, HfoDetection(
+         total_amount=0,
+         frequency=0,
+         plotting_data=PlottingData(
+             detections=[0, 0],
+             analyzed_times=[0.2, 0.3],
+             periods=Periods(
+                 start=[],
+                 stop=[]
+             )
+         )))]
 )
 def test_hfo_detection(duration, spike_monitor, original_time_vector, step_size, window_size, expected_hfo_detection):
     hfo_detection = detect_hfo(duration, spike_monitor,
--- tests/integration/test_hfo_detection_from_dataset.py
+++ tests/integration/test_hfo_detection_from_dataset.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+
+from scipy.signal.spectral import periodogram
+from snn_hfo_ieeg.functions.hfo_detection import HfoDetection, Periods, PlottingData
 import pytest
 from snn_hfo_ieeg.stages.shared_config import Configuration, MeasurementMode
 from run_test_snn_ieeg import CustomOverrides, run_hfo_detection_for_all_channels
@@ -26,12 +29,19 @@ def _generate_test_configuration(dataset_name):
 
 
 def _assert_dummy_hfo_is_empty(hfo_detection):
-    expected_hfo_detection = {'total_hfo': 0, 'time':
-                              [0., 0.0005, 0.001, 0.0015,
-                               0.002, 0.0025, 0.003, 0.0035,
-                               0.004, 0.0045],
-                              'signal': [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-                              'periods_hfo': [[], []]}
+    expected_hfo_detection = HfoDetection(
+        total_amount=0,
+        frequency=0,
+        plotting_data=PlottingData(
+            detections=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            analyzed_times=[0., 0.0005, 0.001, 0.0015,
+                            0.002, 0.0025, 0.003, 0.0035,
+                            0.004, 0.0045],
+            periods=Periods(
+                start=[],
+                stop=[]
+            )
+        ))
     assert are_hfo_detections_equal(expected_hfo_detection, hfo_detection)
 
 
@@ -43,7 +53,7 @@ def test_dummy_data():
 
 
 def _generate_add_detected_hfo_to_list_cb(list):
-    return lambda hfo_detection: list.append(hfo_detection) if hfo_detection['total_hfo'] != 0 else None
+    return lambda hfo_detection: list.append(hfo_detection) if hfo_detection.total_amount != 0 else None
 
 
 def test_hfo_data():
@@ -54,6 +64,7 @@ def test_hfo_data():
         hfo_cb=_generate_add_detected_hfo_to_list_cb(detected_hfos))
     assert len(detected_hfos) == 1
     hfo = detected_hfos[0]
-    assert hfo['total_hfo'] == 1
-    assert hfo['periods_hfo'][0] == [pytest.approx(0)]
-    assert hfo['periods_hfo'][1] == [pytest.approx(0.06)]
+    assert hfo.total_amount == 1
+    assert hfo.frequency == pytest.approx(1)
+    assert hfo.plotting_data.periods.start == [pytest.approx(0)]
+    assert hfo.plotting_data.periods.stop == [pytest.approx(0.06)]
