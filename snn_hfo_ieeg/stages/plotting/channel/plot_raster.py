a246594ee85f2273eaacaa39f2e8a1ac99c4d596
Jan Nils Ferner <contact@jnferner.com>
2021-07-29 18:00:05 +0200

    Actually show raster plot on detection

--- snn_hfo_ieeg/stages/plotting/channel/plot_raster.py
+++ snn_hfo_ieeg/stages/plotting/channel/plot_raster.py
@@ -1,9 +1,12 @@
-from matplotlib.pyplot import plot, xlabel, ylabel
+import matplotlib.pyplot as p
 from brian2.units import ms
 
 
 def plot_raster(hfo_detection):
-    plot(hfo_detection.analytics.spike_times/ms,
-         hfo_detection.analytics.neuron_ids, '.k')
-    xlabel('Time (ms)')
-    ylabel('Neuron index')
+    if hfo_detection.result.total_amount == 0:
+        return
+    p.plot(hfo_detection.analytics.spike_times/ms,
+           hfo_detection.analytics.neuron_ids, '.k')
+    p.xlabel('Time (ms)')
+    p.ylabel('Neuron index')
+    p.show()
