a8e8f69a2a1c1f2eba3c3c614cb6c5e0bcfb2cef
Jan Nils Ferner <contact@jnferner.com>
2021-08-03 13:31:03 +0200

    Fix dimension mismatch

--- tests/utility.py
+++ tests/utility.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
 def assert_are_lists_approximately_equal(first_list, second_list, accuracy=None):
-    assert np.all(first_list == [pytest.approx(_, abs=accuracy) for _ in second_list])
+    assert np.all(np.array(first_list, dtype=second_list.dtype) == [
+                  pytest.approx(_, abs=accuracy) for _ in second_list])
 
 
 def assert_are_hfo_detections_equal(first_hfo, second_hfo):
@@ -12,7 +13,8 @@ def assert_are_hfo_detections_equal(first_hfo, second_hfo):
         assert_are_lists_approximately_equal(
             property_cb(first_hfo), property_cb(second_hfo))
     assert first_hfo.result.total_amount == second_hfo.result.total_amount
-    assert first_hfo.result.frequency == pytest.approx(second_hfo.result.frequency)
+    assert first_hfo.result.frequency == pytest.approx(
+        second_hfo.result.frequency)
     assert_are_values_same(lambda hfo: hfo.analytics.analyzed_times)
     assert_are_values_same(lambda hfo: hfo.analytics.detections)
     assert_are_values_same(lambda hfo: hfo.analytics.periods.start)
