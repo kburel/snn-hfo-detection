07096a8f298a43f516c1bff7cf5b5880f3733440
Jan Nils Ferner <contact@jnferner.com>
2021-08-03 17:41:45 +0200

    Simplify deserialization

--- snn_hfo_ieeg/stages/persistence/loading.py
+++ snn_hfo_ieeg/stages/persistence/loading.py
@@ -24,17 +24,15 @@ def _deserialize_inner_matlab(item, type: NamedTuple):
         value = params[property_name]
         property_type = outer_property_type.__args__[0] if _is_optional(
             outer_property_type) else outer_property_type
-        if np.all(value == 'None'):
+        if str(value[0]) == 'None':
             params[property_name] = None
         elif _has_annotations(property_type):
             params[property_name] = _deserialize_inner_matlab(
                 value, property_type)
-        elif isinstance(value, (list, np.ndarray)) and isinstance(value[0], (list, np.ndarray)):
-            params[property_name] = property_type(value[0])
         elif property_type is int:
-            params[property_name] = int(float(value))
+            params[property_name] = int(float(value[0]))
         else:
-            params[property_name] = property_type(value)
+            params[property_name] = property_type(value[0])
     return type(**params)
 
 
