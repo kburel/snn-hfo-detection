c01702805e63329b00c3f0ef64fd0a398b9e2433
Jan Nils Ferner <contact@jnferner.com>
2021-08-04 12:56:20 +0200

    Put channel labels in right format

--- snn_hfo_ieeg/stages/loading/patient_data.py
+++ snn_hfo_ieeg/stages/loading/patient_data.py
@@ -1,6 +1,8 @@
 from typing import NamedTuple
 import numpy as np
 import scipy.io as sio
+import h5py
+from scipy.io.matlab.mio5_utils import chars_to_strings
 
 
 class PatientData(NamedTuple):
@@ -21,12 +23,11 @@ class ChannelData(NamedTuple):
 
 
 def load_patient_data(full_intervals_path):
-    interval = sio.loadmat(full_intervals_path)
-    print(interval)
+    interval = sio.loadmat(full_intervals_path, chars_as_strings=True)
     return PatientData(
         wideband_signals=interval['channels'],
         signal_time=interval['times'][0],
-        channel_labels=interval['None']['channel_labels'][0])
+        channel_labels=interval['channel_labels'])
 
 
 def extract_channel_data(patient_data, channel):
