1741a93fa9b8d494299b5c4ed20e4fe3603ba288
Jan Nils Ferner <contact@jnferner.com>
2021-08-04 17:58:53 +0200

    Remove patients dir from tests

--- tests/stages/loading/test_folder_discovery.py
+++ tests/stages/loading/test_folder_discovery.py
@@ -1,25 +1,35 @@
 import os
+import pytest
 from tests.utility import get_tests_path
-from snn_hfo_ieeg.stages.loading.folder_discovery import Patients, get_patient_interval_paths
+from snn_hfo_ieeg.stages.loading.folder_discovery import get_interval_paths
 
 
-def test_filters_patient_directories():
+def _get_test_data_path():
     tests_path = get_tests_path()
-    test_data_path = os.path.join(tests_path, 'stages', 'loading', 'test_data')
-    patients = get_patient_interval_paths(test_data_path)
-    expected_patients: Patients = {
-        0: {
-            0: os.path.join(test_data_path, 'P0', 'I0.mat'),
-        },
-        1: {
-            1: os.path.join(test_data_path, 'P1', 'I1.mat'),
-            2: os.path.join(test_data_path, 'P1', 'I2.mat'),
-        },
-        2: {
-            1: os.path.join(test_data_path, 'P2', 'I1.mat'),
-        },
-        4: {
-            5: os.path.join(test_data_path, 'P4', 'I5.mat'),
-        }
-    }
-    assert expected_patients == patients
+    return os.path.join(
+        tests_path, 'stages', 'loading', 'test_data')
+
+
+def _get_patient_path(patient):
+    test_data_path = _get_test_data_path()
+    return os.path.join(test_data_path, f'P{patient}')
+
+
+def _get_interval_path(patient, interval):
+    patient_path = _get_patient_path(patient)
+    return os.path.join(patient_path, f'I{interval}.mat')
+
+
+@pytest.mark.parametrize(
+    'input_path, expected_intervals',
+    [(_get_patient_path(0), {0: _get_interval_path(0, 0)}),
+     (_get_patient_path(1), {1: _get_interval_path(1, 1),
+                             2: _get_interval_path(1, 2)}),
+     (_get_patient_path(2), {1: _get_interval_path(2, 1)}),
+     (_get_patient_path(3), {5: _get_interval_path(3, 5)}),
+     (_get_patient_path(4), {}),
+     (_get_test_data_path(), {})]
+)
+def test_filters_patient_directories(input_path, expected_intervals):
+    intervals = get_interval_paths(input_path)
+    assert expected_intervals == intervals
