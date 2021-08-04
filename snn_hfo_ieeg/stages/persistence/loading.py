d1b642cf01bacc05ec5a309de4ad78d8549d5d55
Jan Nils Ferner <contact@jnferner.com>
2021-08-04 21:58:23 +0200

    Save data using pickle

--- snn_hfo_ieeg/stages/persistence/loading.py
+++ snn_hfo_ieeg/stages/persistence/loading.py
@@ -1,52 +1,7 @@
-from typing import NamedTuple
+import pickle
 from os import path
-import scipy.io as sio
 from snn_hfo_ieeg.user_facing_data import HfoDetectionWithAnalytics
 from snn_hfo_ieeg.stages.persistence.utility import get_persistence_path
-
-
-def _has_annotations(type) -> bool:
-    return hasattr(type, '__annotations__')
-
-
-def _is_optional(type) -> bool:
-    return (
-        hasattr(type, "__args__")
-        and len(type.__args__) == 2
-    )
-
-
-def _deserialize_inner_matlab(item, type: NamedTuple):
-    values = item[0][0]
-    names = item.dtype.names
-    params = dict(zip(names, values))
-    for property_name, outer_property_type in type.__annotations__.items():
-        value = params[property_name]
-        property_type = outer_property_type.__args__[0] if _is_optional(
-            outer_property_type) else outer_property_type
-        if str(value[0]) == 'None':
-            params[property_name] = None
-        elif _has_annotations(property_type):
-            params[property_name] = _deserialize_inner_matlab(
-                value, property_type)
-        elif property_type is int:
-            params[property_name] = int(float(value[0]))
-        else:
-            params[property_name] = property_type(value[0])
-    return type(**params)
-
-
-def _deserialize_matlab(dictionary: dict, type: NamedTuple) -> HfoDetectionWithAnalytics:
-    params = {}
-    for key, item in dictionary.items():
-        item_type = type.__annotations__[key]
-        params[key] = _deserialize_inner_matlab(item, item_type)
-    return type(**params)
-
-
-def _remove_matlab_keys(dictionary):
-    for key in ['__header__', '__version__', '__globals__']:
-        del dictionary[key]
 
 
 def load_hfo_detection(loading_path, metadata) -> HfoDetectionWithAnalytics:
@@ -54,6 +9,5 @@ def load_hfo_detection(loading_path, metadata) -> HfoDetectionWithAnalytics:
     if not path.isfile(filepath):
         raise ValueError(
             f'No HFO detection data was saved for patient {metadata.patient}, interval {metadata.interval}, channel {metadata.channel}')
-    dictionary = sio.loadmat(filepath)
-    _remove_matlab_keys(dictionary)
-    return _deserialize_matlab(dictionary, HfoDetectionWithAnalytics)
+    with open(filepath, 'rb') as file:
+        return pickle.load(file)
--- snn_hfo_ieeg/stages/persistence/utility.py
+++ snn_hfo_ieeg/stages/persistence/utility.py
@@ -4,6 +4,6 @@ import os
 def get_persistence_path(saving_path, metadata) -> str:
     parent_directory = os.path.join(saving_path,
                                     f'I{metadata.interval}')
-    filename = f'C{metadata.channel}.mat'
+    filename = f'C{metadata.channel}.p'
 
     return os.path.join(parent_directory, filename)
