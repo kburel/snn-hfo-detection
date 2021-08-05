d832fcad26e3493d452635d18813d34298b881be
Jan Nils Ferner <contact@jnferner.com>
2021-08-05 09:16:40 +0200

    Fix JSON dumping

--- snn_hfo_ieeg/stages/persistence/loading.py
+++ snn_hfo_ieeg/stages/persistence/loading.py
@@ -1,5 +1,6 @@
 import json
 from os import path
+from types import SimpleNamespace
 from snn_hfo_ieeg.user_facing_data import HfoDetectionWithAnalytics
 from snn_hfo_ieeg.stages.persistence.utility import get_persistence_path
 
@@ -10,4 +11,4 @@ def load_hfo_detection(loading_path, metadata) -> HfoDetectionWithAnalytics:
         raise ValueError(
             f'No HFO detection data was saved for patient {metadata.patient}, interval {metadata.interval}, channel {metadata.channel}')
     with open(filepath, 'r') as file:
-        return json.load(file)
+        return json.load(file, object_hook=lambda d: SimpleNamespace(**d))
--- snn_hfo_ieeg/stages/persistence/saving.py
+++ snn_hfo_ieeg/stages/persistence/saving.py
@@ -1,5 +1,6 @@
 import os
 import json
+import numpy as np
 from pathlib import Path
 from snn_hfo_ieeg.stages.persistence.utility import get_persistence_path
 
@@ -9,8 +10,49 @@ def _create_parent_directory(path):
     os.makedirs(parent_directory, exist_ok=True)
 
 
+def _is_namedtuple(obj) -> bool:
+    # Source: https://stackoverflow.com/a/62692640/5903309
+    return (
+        isinstance(obj, tuple) and
+        hasattr(obj, '_asdict') and
+        hasattr(obj, '_fields')
+    )
+
+
+def _is_list(obj) -> bool:
+    return (
+        hasattr(obj, '__iter__') and
+        not isinstance(obj, (str, dict))
+    )
+
+
+def _is_dict(obj) -> bool:
+    return isinstance(obj, dict)
+
+
+def _is_np_bool(obj) -> bool:
+    return isinstance(obj, np.bool_)
+
+
+def _convert_to_dict(object):
+    if _is_namedtuple(object):
+        return _convert_to_dict(object._asdict())
+    if _is_list(object):
+        return [_convert_to_dict(_) for _ in object]
+    if _is_dict(object):
+        for key, item in object.items():
+            object[key] = _convert_to_dict(item)
+        return object
+    if _is_np_bool(object):
+        return bool(object)
+    if object is None:
+        return str(object)
+    return object
+
+
 def save_hfo_detection(user_facing_hfo_detection, saving_path, metadata):
     filepath = get_persistence_path(saving_path, metadata)
     _create_parent_directory(filepath)
+    dictionary = _convert_to_dict(user_facing_hfo_detection)
     with open(filepath, 'w') as file:
-        json.dump(user_facing_hfo_detection, file)
+        json.dump(dictionary, file)
