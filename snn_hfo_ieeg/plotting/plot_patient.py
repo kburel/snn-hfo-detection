0d0efbfbfdab68919bde8b0ee0add170644bb8c2
Jan Nils Ferner <contact@jnferner.com>
2021-08-05 09:44:27 +0200

    Make plot prettier

--- snn_hfo_ieeg/plotting/plot_patient.py
+++ snn_hfo_ieeg/plotting/plot_patient.py
@@ -3,7 +3,6 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from snn_hfo_ieeg.user_facing_data import HfoDetectionWithAnalytics, Metadata
-color_bars = ['#2f70b6', '#c85a2d', '#6f63b6', '#e26a84']
 
 
 class ChannelData(NamedTuple):
@@ -36,22 +35,46 @@ def _append_or_create(dict, key, value):
 
 
 def plot_mean_hfo_rate(intervals: Intervals):
+    if len(intervals) == 0:
+        return
+
     label_to_hfo_rates = {}
-    print(intervals)
     for channels in intervals.values():
         for channel in channels:
             _append_or_create(
                 dict=label_to_hfo_rates,
                 key=channel.metadata.channel_label,
                 value=channel.hfo_detection.result.frequency * 60)
-    labels = label_to_hfo_rates.keys()
+
+    labels = list(label_to_hfo_rates.keys())
     hfo_rates = label_to_hfo_rates.values()
     mean_hfo_rates = [mean(_) for _ in hfo_rates]
-    standard_deviations = [np.std(_) for _ in hfo_rates]
-    plt.ylabel('Average HFO rate per minute')
-    plt.xlabel('Channel')
-    plt.bar(
+    standard_deviations = [np.std(_) for _ in hfo_rates] \
+        if len(intervals) > 1 else None
+
+    figure_size = (15, 5)
+    fig, axes = plt.subplots(figsize=figure_size)
+    plt.rc('font', family='sans-serif')
+    plt.tight_layout()
+    fig.subplots_adjust(wspace=0.2, hspace=0.2)
+
+    axes.bar(
         x=labels,
         height=mean_hfo_rates,
+        edgecolor='k',
+        ecolor='#0218f5',
+        alpha=0.9, color='#2f70b6',
         yerr=standard_deviations,
         capsize=2)
+
+    axes.set_xticklabels(labels, rotation=45,
+                         fontsize=16, horizontalalignment='right')
+
+    axes.set_xlabel('Electrode label', fontsize=18)
+    axes.set_ylabel('HFO rate (event/min)', fontsize=18)
+
+    axes.tick_params(axis='y', labelsize=16, length=8)
+    axes.tick_params(axis='x', labelsize=16, length=8)
+
+    axes.spines['top'].set_visible(False)
+    axes.spines['right'].set_visible(False)
